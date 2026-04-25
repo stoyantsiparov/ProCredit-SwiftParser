4b4ea765919a83d0794871592dbafeb9783bae42
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-25 08:56:44 +0300

    Register the SwiftParserService for dependency injection

--- SwiftParser.WebApi/Program.cs
+++ SwiftParser.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using SwiftParser.WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Register the SwiftParserService for dependency injection
+builder.Services.AddScoped<ISwiftParserService, SwiftParserService>();
 
 var app = builder.Build();
 
